c67ffe81785f7f4ec0977028437dc7cb2a4e2258
NAO <naox94xichigo@ezweb.ne.jp>
2023-01-15 22:54:52 +0900

    20230115pm

---
+++ js/test.js
@@ -0,0 +1,10 @@
+const showMessage = () => {
+    const namebox = document.getElementById("input-name");
+alert(namebox.value);
+    }
+
+System.out.print("isEmptyメソッドで判定→");
+    if (str.isEmpty()) {
+
+    System.out.println("strは空白文字");
+  }
