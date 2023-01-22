a26fbbcaaaa68059eec2ff753e83cce8c0345d10
NAO <naox94xichigo@ezweb.ne.jp>
2023-01-22 11:28:57 +0900

    20230122am

--- js/test.js
+++ js/test.js
@@ -1,10 +1,22 @@
-const showMessage = () => {
-    const namebox = document.getElementById("input-name");
-alert(namebox.value);
+const btn = document.getElementById('input-name');
+const btn1 = document.getElementById('input-mail');
+const btn2 = document.getElementById('input-tel');
+const btn3 = document.getElementById("input-text");
+function check(){
+    if(btn.value == ""){
+        alert("名前を入力してください");
+	return false;
+    }else if(btn1.value ==""){
+        alert("メールアドレスを入力してください");
+    return false;
+    }else if(btn2.value ==""){
+        alert("電話番号を入力してください");
+	return false;
+    }else if(btn3.value ==""){
+        alert("問合内容を入力してください");
+	return false;
+    }else{
+	return true;
     }
+}
 
-System.out.print("isEmptyメソッドで判定→");
-    if (str.isEmpty()) {
-
-    System.out.println("strは空白文字");
-  }
